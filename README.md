38ad3762ba31ebf3d14fcc1ab127f32f71e3035f
Johannes Scribante <johannes.scribs@gmail.com>
2023-11-29 16:02:52 +0200

    task/0 - added docs and changelog

---
+++ README.md
@@ -0,0 +1,17 @@
+# serve go package
+
+The `serve` package contains several subpackages that help the the building of
+API services.
+
+### Auth
+
+WIP: A basic authentication middleware package.
+
+### OpenAPI
+
+Parses your `openapi.yaml` file and provides a handler to render your docs.
+
+### ServeMux
+
+The `servemux` package is a very light-weight and simple multiplexer for
+handling and building API's.
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"github.com/dottics/serve/auth"
+	"github.com/dottics/serve/openapi"
+	"github.com/dottics/serve/servemux"
+	"log"
+	"net/http"
+)
+
+func Home(c *context.Context, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(204)
+	_, _ = w.Write([]byte(`home`))
+}
+
+func openAPI(c *context.Context, w http.ResponseWriter, r *http.Request) {
+	openapi.Handler(w, r)
+}
+
+func main() {
+	mux := servemux.NewMux()
+	mux.Use(servemux.CORS(nil, nil))
+	mux.Use(auth.Middleware)
+	// whitelisted
+	mux.GetWhitelisted("/docs*", openAPI)
+
+	// for testing
+	//	mux.PostWhitelisted("/queue")
+
+	// protected
+	mux.Get("/", Home)
+	//mux.PostWhitelisted("/queue", handlers.ProactiveMessageQueue)
+
+	log.Println("server listening on port: 8000")
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
